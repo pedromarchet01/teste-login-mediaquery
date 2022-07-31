6d6239fedc7ea0c0d784e884a0c47f241b6a9a34
PEDRO MARCHET <100133140+pedromarchet01@users.noreply.github.com>
2022-07-31 17:10:50 -0300

    Add files via upload

---
+++ PROJETO-LOGIN/js/loginscript.js
@@ -0,0 +1,13 @@
+const userInput = document.querySelector("#user");
+const senhaInput = document.querySelector("#senha");
+const submitButton = document.querySelector(".button");
+
+submitButton.addEventListener('click', (e) => { e.preventDefault()
+
+  const userValue.userInput.value;
+  const senhaValue.senhaInput.value;
+
+  if (userValue === '' || senhaValue === '') {
+     
+  }
+})
